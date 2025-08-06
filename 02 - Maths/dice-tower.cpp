ee92fcb05b557490569f2d81ae2f883afe168c25
adi <smilingaditya12@gmail.com>
2025-08-06 16:50:45 +0530

    maths
    constructive algorithm

---
+++ 02 - Maths/dice-tower.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        long long x;
+        cin>>x;
+        if(x>14&&x%14>=1&&x%14<=6){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
