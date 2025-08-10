8e36a2eb31a3b5d8ccf8f054b4baee7986ec4b5f
adi <smilingaditya12@gmail.com>
2025-08-10 22:52:17 +0530

    maths

---
+++ 02 - Maths/divisibility-problem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b;
+        cin >> a >> b;
+        if (a % b == 0)
+            cout << 0 << "\n";
+        else
+            cout << b - (a % b) << "\n";
+    }
+    return 0;
+}
